5c41af73c117aa0a12883fffa62fb50035e94e50
Khanh Tran <57286986+khanhdk0000@users.noreply.github.com>
2023-09-10 07:29:17 +0700

    Add dto

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { title } from 'process';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.taskService.getAllTasks();
+  }
+
+  // whatever input will be parsed as body
+
+  // @Post()
+  // createTask(@Body('title') title, @Body('description') description): Task {
+  //   return this.taskService.createTask(title, description);
+  // }
+
+  @Post()
+  createTask(@Body() createTaskDTO: CreateTaskDTO): Task {
+    return this.taskService.createTask(createTaskDTO);
   }
 }
--- task-management/src/tasks/tasks.service.ts
+++ task-management/src/tasks/tasks.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
-  getAllTasks() {
+  private tasks: Task[] = [];
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(createTaskDTO: CreateTaskDTO): Task {
+    // const { title, description } = createTaskDTO;
+
+    const task: Task = {
+      id: uuid(),
+      title: createTaskDTO.title,
+      description: createTaskDTO.description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
