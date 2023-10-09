8fa9c0e8b27963651065398f73a12868f944557c
Khanh Tran <khanhtran28092000@gmail.com>
2023-10-09 09:12:45 +0000

    nest-official

---
+++ nest-official/src/coffee/coffee.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('coffee')
+export class CoffeeController {
+    
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return id;
+  }
+}
