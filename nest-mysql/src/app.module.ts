c501fae8dca99e6e764e13ba9d85066a8a48c33d
Khanh Tran <khanhtran28092000@gmail.com>
2023-10-09 07:04:07 +0000

    no idea

--- nest-mysql/src/app.module.ts
+++ nest-mysql/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '172.17.0.3',
+      port: 3306,
+      username: 'root',
+      password: 'a',
+      database: 'nestjs_test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
