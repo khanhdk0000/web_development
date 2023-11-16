1ea84b261990756f95c2cfc2dd968389b8cd21b1
Khanh Tran <khanhtran28092000@gmail.com>
2023-11-16 04:19:49 +0000

    nestjs session

--- nest-mysql/src/main.ts
+++ nest-mysql/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      name: 'nest_session',
+      secret: 'qwerty',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
   await app.listen(8000);
 }
 bootstrap();
