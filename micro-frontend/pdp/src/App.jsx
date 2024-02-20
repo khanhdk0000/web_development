a323ea4f3bea5b841749789904914cbdc5114a03
Khanh Tran <khanhtran28092000@gmail.com>
2024-02-20 15:41:08 +0000

    Error handling mfe

--- micro-frontend/pdp/src/App.jsx
+++ micro-frontend/pdp/src/App.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import Header from "home/Header";
+const Header = React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
+import SafeComponent from "./SafeComponent";
 
-const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10"> Pdp page content </div>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [header, setHeader] = useState(true);
+  return (
+    <div className="text-3xl mx-auto max-w-6xl">
+      {header &&
+        <SafeComponent>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Header />
+          </Suspense>
+        </SafeComponent>
+      }
+      <button className="text-3xl p-5" onClick={() => setHeader(!header)}>Toggle Header</button>
+      <div className="my-10"> Pdp page content </div>
+      <Footer />
+    </div>);
+};
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ micro-frontend/pdp/src/SafeComponent.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class SafeComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        /* We can log the error to an error reporting service  */
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+        return this.props.children;
+    }
+
+}
